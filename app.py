e6c37f798df9f7334d7983e7b51e6d18d32346cf
isom3400 <isom3400@ggmail.ust.hk>
2026-04-20 17:46:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,49 @@ st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
 
-st.write(['list1', 'list2'])
+st.write(['list1', 
+          'list2'])
 
 st.header("Section 2: MarkDown")
 st.write("**Bold Text** and *Italic Text*")
 
 
+
+
+Choose IPYNB Output Option:
+
+
+Show
+
+
+Hide
+
+import streamlit as st
+
+
+st.title()
+st.title("Welcome to Streamlit!")
+
+
+st.write()
+st.write("Hello, Streamlit!")
+st.write(12345)
+st.write({"Name": "Alice", "Age": 30})
+
+
+May also use Markdown
+st.write("**Bold Text** and *Italic Text*")
+
+
+st.header()
+st.header("Section 1: Introduction")
+
+
+st.number_input()
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
 
 
 
