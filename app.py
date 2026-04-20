c1da24163db08e7c8d883464a7078addc560f2cc
isom3400 <isom3400@ggmail.ust.hk>
2026-04-20 17:40:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title("Welcome to Streamlit!")
+
+st.write("Hello, Streamlit!")
+st.write(12345)
+st.write({"Name": "Alice", "Age": 30})
+
+st.write(['list1', 'list2'])
+
+
+
