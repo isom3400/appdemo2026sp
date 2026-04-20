46655890edd99e7818bae5909b74643b94ae0ada
isom3400 <isom3400@ggmail.ust.hk>
2026-04-20 17:44:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ import streamlit as st
 
 st.title("Welcome to Streamlit!")
 
+st.header("Section 1: Introduction")
 st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
 
 st.write(['list1', 'list2'])
 
+st.header("Section 2: MarkDown")
 st.write("**Bold Text** and *Italic Text*")
+
+
+
+
 
 
 
