47e801987f5e041610f7777b45ebba6d5bab4380
isom3400 <isom3400@ggmail.ust.hk>
2026-04-24 12:29:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ st.header("ISOM3400")
 
 st.write("**Bold Text** and *Italic Text*")
 
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
+st.write(f"Your age is {age}")
