26605ddb42417af13068d107d7c259a7a0485ceb
isom3400 <isom3400@ggmail.ust.hk>
2026-04-24 12:31:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ age = st.number_input("Enter your age:",
                       max_value=120,
                       value=25)
 st.write(f"Your age is {age}")
+
+option = st.selectbox("Choose your favorite color:",
+                      ["Red", "Blue", "Green"])
+st.write(f"You selected: {option}")
+
+
+
+
+
+
+
+
